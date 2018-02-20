0ebe7bfd2eb30a48d8b86399d57eea50bc7b8d82
alex-osi <osipchuk.alex@gmail.com>
2018-02-20 17:21:51 +0800

    20.02

--- js/animate.js
+++ js/animate.js
@@ -1,31 +1,5 @@
 $(document).ready(function() {
 
-    // $('.sl').slick({
-    //     slidesToShow: 1,
-    //     slidesToScroll: 1,
-    //     arrows: false,
-    //     fade: true,
-    //     cssEase: 'ease-in',
-    //     asNavFor: '.sl1'
-    // });
-    // $('.sl1').slick({
-    //     slidesToShow: 5,
-    //     slidesToScroll: 1,
-    //     asNavFor: '.sl',
-    //     dots: true,
-    //     // centerPadding: '40px',
-    //     centerMode: true,
-    //     focusOnSelect: true,
-    //     autoplay: true,
-    //     autoplaySpeed: 3000,
-    //     responsive: [{
-    //                 breakpoint: 768,
-    //                 settings: {
-    //                     slidesToShow: 3,
-    //                 }
-    //             },]
-    // });
-
 //responsive
     $('.slider').slick({
 
@@ -76,6 +50,28 @@ $('.slider-1').slick({
     }]
 
 });
+
+    $('.slider-2').slick({
+
+        responsive: [{
+
+            breakpoint: 3500,
+            settings: {
+                slidesToShow: 4
+            }
+
+        }, {
+
+            breakpoint: 767,
+            settings: {
+                slidesToScroll: 1,
+                slidesToShow: 1,
+                dots: false
+            }
+
+        }]
+
+    });
 
     $('.review-slider').slick({
 
--- js/main.js
+++ js/main.js
@@ -51,58 +51,47 @@ $(".slide-popup6").magnificPopup({
     }
 });
 
+$(".slide-popup7").magnificPopup({
+    type : 'image',
+    gallery : {
+        enabled : true
+    }
+});
 
-// $(document).ready(function() {
+$(".slide-popup8").magnificPopup({
+    type : 'image',
+    gallery : {
+        enabled : true
+    }
+});
 
-    //popUp
-    // $(".slider").magnificPopup({
-    //     delegate: 'a',
-    //     type : 'image',
-    //     gallery : {
-    //         enabled : true
-    //     }
-    // });
+$(".slide-popup9").magnificPopup({
+    type : 'image',
+    gallery : {
+        enabled : true
+    }
+});
 
-    //E-mail Ajax Send
-//     $("form").submit(function() { //Change
-//         var th = $(this);
-//         $.ajax({
-//             type: "POST",
-//             url: "mail.php", //Change
-//             data: th.serialize()
-//         }).done(function() {
-//             alert("Спасибо! Скоро я с Вами свяжусь");
-//             setTimeout(function() {
-//                 // Done Functions
-//                 th.trigger("reset");
-//                 // $.magnificPopup.close();
-//             }, 1000);
-//         });
-//         return false;
-//     });
-//
-// });
+$(".slide-popup10").magnificPopup({
+    type : 'image',
+    gallery : {
+        enabled : true
+    }
+});
 
-// Фильтрация блоков с помощью jQuery
-// var fActive = '';
-//
-// function filterColor(color){
-//     if(fActive != color){
-//         $('#fil-1 div').filter('.'+color).slideDown();
-//         $('#fil-1 div').filter(':not(.'+color+')').slideUp();
-//         fActive = color;
-//     }
-// }
-//
-// $('.f-red').click(function(){ filterColor('red'); });
-// $('.f-blue').click(function(){ filterColor('blue'); });
-// $('.f-green').click(function(){ filterColor('green'); });
-//
-// $('.f-all').click(function(){
-//     $('#fil-1 div').slideDown();
-//     fActive = 'all';
-// });
+$(".slide-popup11").magnificPopup({
+    type : 'image',
+    gallery : {
+        enabled : true
+    }
+});
 
+$(".slide-popup12").magnificPopup({
+    type : 'image',
+    gallery : {
+        enabled : true
+    }
+});
 
 //кнопка Вверх
 $(window).scroll(function() {
@@ -116,13 +105,10 @@ $('.top').click(function() {
     $('html , body').stop().animate({scrollTop: 0}, 'slow', 'swing');
 });
 
-
-// $('#exampleModal').on('show.bs.modal', function (event) {
-//     var button = $(event.relatedTarget) // Button that triggered the modal
-//     var recipient = button.data('') // Extract info from data-* attributes
-//     // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
-//     // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
-//     var modal = $(this)
-//     modal.find('.modal-title').text('New message to ')
-//     modal.find('.modal-body input').val(recipient)
-// })
+// Скролл по якорю
+$('a[data-target^="anchor"]').bind('click.smoothscroll',function () {
+    var target = $(this).attr('href'),
+        bl_top = $(target).offset().top - 70;
+    $('body, html').animate({scrollTop: bl_top}, 1100);
+    return false;
+});
